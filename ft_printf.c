b4a8889d50a23ff40f1f163b5fcf0d3000adacd7
Mikhail Filipchuk <mfilipch@e1z2r9p29.42.us.org>
2016-11-20 21:14:34 -0800

    misc

--- ft_printf.c
+++ ft_printf.c
@@ -13,6 +13,7 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include "libft.h"
+#include <stdio.h>
 
 int			ft_printf(const char *fmt, ...)
 {
@@ -22,11 +23,16 @@ int			ft_printf(const char *fmt, ...)
 	int				l;
 	int				len;
 	int				d;
+	int				sign;
 	char			*s;
+	int				res;
 
 	va_start(ap, fmt);
 	width = 0;
 	prec = -1;
+	res = 0;
+	if (!fmt)
+		return (-1);
 	while (*fmt)
 	{
 		if ((*fmt == '%' && *(fmt++)) || width > 0 || prec >= 0)
@@ -35,22 +41,32 @@ int			ft_printf(const char *fmt, ...)
 			{
 				// write(1,"TEST2\n", 6);
 				s = va_arg(ap, char *);
-				len = (prec >= 0 && prec < len) ? prec : ft_strlen(s);
+				len = ft_strlen(s);
+				len = (prec >= 0 && prec < len) ? prec : len;
 				l = len - 1;
 				while (++l < width)
 					ft_putchar(' ');
 				write(1, s, len);
+				res += len + ((width > len) ? (width - len) : 0);
 			}
 			// else if (*fmt == 'S')
 				//s with widechar flag l
 			else if (*fmt == 'd' || *fmt == 'i')
 			{
 				d = va_arg(ap, int);
-				len = (prec >= 0 && prec < len) ? prec : strlen(ft_itoa(d));
-				l = (len) - 1;
+				sign = (d < 0) ? -1 : 1; 
+				// len = (prec >= 0 && ((width > 0 && prec < width) || !width)) ? prec : strlen(ft_itoa(d));
+				len = strlen(ft_itoa(d)) + (sign - 1) / 2;
+				//len = (prec >= 0 && prec < len) ? prec : len;
+				l = ((prec > len) ? prec : len) - 1;
 				while (++l < width)
 					ft_putchar(' ');
-				ft_putnbr(d);
+				(sign < 0) ? ft_putchar('-') : 0;
+				l = len - 1;
+				while (++l < prec)
+					ft_putchar('0');
+				ft_putnbr(d * sign);
+				res += len - (sign - 1) / 2 + ((width > len  && width >= prec) ? (width - len) : 0) + ((prec > len && prec > width) ? (prec - len) : 0);
 			}
 			else if (*fmt == 'c')
 				ft_putchar((char)va_arg(ap, int));
@@ -73,26 +89,33 @@ int			ft_printf(const char *fmt, ...)
 			// else if (*fmt == 'p')
 			// 	pointer printed in hex as if %#x;
 			else if (*fmt == '%')
-				ft_putchar(*fmt);
-			if (*fmt == '.')
 			{
+				ft_putchar(*fmt);
+				res++;
+			}
+			else if (*fmt == '.')
+			{
+				// printf("%s\n", "TEST");
 				prec = ft_atoi(fmt + 1);
-				fmt += strlen(ft_itoa(prec));
+				if (prec > 0)
+					fmt += strlen(ft_itoa(prec));
 			}
 			else if (prec >= 0)
 				prec = -1;
-			if (prec == -1 && ft_isdigit(*fmt) && (width = ft_atoi(fmt)))
+			else if (prec == -1 && ft_isdigit(*fmt) && (width = ft_atoi(fmt)))
 				fmt += strlen(ft_itoa(width)) - 1;
 			else if (prec == -1 && width > 0)
-				width = 0; 			
-			// else
-			// 	return (1);
-			
+				width = 0;
+			else
+				return (-1);			
 		}
 		else
+		{
 			ft_putchar(*fmt);
+			res++;
+		}
 		fmt++;
 	}
 	va_end(ap);
-	return (0);
+	return (res);
 }
--- main.c
+++ main.c
@@ -17,29 +17,66 @@ int	ft_printf(const char *fmt, ...);
 
 int main(int argc, char const **argv)
 {
+	int a[10];
+	int	b[10];
+	int	n;
+	int	i;
+
 	if (argc < 2 && argv[0])
 		return (0);
-	ft_printf("You:\n");
+	// ft_printf("You:\n");
 	// ft_printf("Str %s | Num %%d | Char %c\n", argv[1], atoi(argv[2]), argv[3][0]);
 	// ft_printf(":%15s:\n", "Hello, world!");
 	// ft_printf(":%.10s:\n", "Hello, world!");
 	// ft_printf(":%.15s:\n", "Hello, world!");
 	// ft_printf(":%15.10s:\n", "Hello, world!");
-	// ft_printf("%5d\n", 42);
-	ft_printf("%d", 42);
-	ft_printf("%i", 42);
-	ft_printf("This is a simple test.");
-	ft_printf("%u", -42);
-	printf("\nLib:\n");
+	// a[0] = ft_printf("%15.4s", "42");
+	// a[1] = ft_printf("NULL s %s", "");
+	// a[0] = ft_printf("%.4s", "42 is the answer");
+	// a[1] = ft_printf("%.4s", "42");
+	// a[2] = ft_printf("%4.15s", "42 is the answer");
+	// a[3] = ft_printf("%4.s", "42");
+	a[0] = ft_printf("%.4d", 42);
+	a[1] = ft_printf("%.4d", 424242);
+	a[2] = ft_printf("%4.15d\n", 42);
+	a[3] = ft_printf("%15.4d\n", 42);
+	a[4] = ft_printf("%4.15d\n", -42);
+	a[5] = ft_printf("%15.4d\n", -42);
+	// a[1] = ft_printf("%.4s", "42");
+	// a[2] = ft_printf("%4.15s", "42 is the answer");
+	// a[3] = ft_printf("%4.s", "42");
+	ft_printf("\n");
+	// a[0] = ft_printf("%d", 42);
+	// a[1] = ft_printf("%i", 42);
+	// a[2] = ft_printf("This is a simple test.");
+	// a[3] = ft_printf("%u", -42);
+	// printf("\nLib:\n");
 	// printf("Str %s | Num %%d | Char %c\n", argv[1], atoi(argv[2])/*, argv[3][0]*/);
 	// printf(":%15s:\n", "Hello, world!");
 	// printf(":%.10s:\n", "Hello, world!");
 	// printf(":%.15s:\n", "Hello, world!");
 	// printf(":%15.10s:\n", "Hello, world!");
-	// printf("%5d\n", 42);
-	printf("%d", 42);
-	printf("%i", 42);
-	printf("This is a simple test.");
-	printf("%u", -42);
+	// b[0] = printf("%15.4s", "42");
+	// b[1] = printf("NULL s %s", "");
+	// b[0] = printf("%.4s", "42 is the answer");
+	// b[1] = printf("%.4s", "42");
+	// b[2] = printf("%4.15s", "42 is the answer");
+	// b[3] = printf("%4.s", "42");
+	b[0] = printf("%.4d", 42);
+	b[1] = printf("%.4d", 424242);
+	b[2] = printf("%4.15d\n", 42);
+	b[3] = printf("%15.4d\n", 42);
+	b[4] = printf("%4.15d\n", -42);
+	b[5] = printf("%15.4d\n", -42);
+	// b[0] = printf("%d", 42);
+	// b[1] = printf("%i", 42);
+	// b[2] = printf("This is a simple test.");
+	// b[3] = printf("%u", -42);
+	n = 6;
+	i = -1;
+	printf("\n");
+	while (++i < n)
+		if (a[i] != b[i])
+			printf("Test %d FAIL: You %d | Lib %d\n", i, a[i], b[i]);
 	return (0);
 }
